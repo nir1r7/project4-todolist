915b6334039c8004dbd9da14a064b409e63b4f29
Nirvan Rabbani <nir1r7@gmail.com>
2023-03-09 14:57:50 -0500

    worked on the todo list, added the list part of it took an emoji display code from google

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Emoji from './emoji';
 
 class ToDoList extends Component {
     render() { 
@@ -11,10 +12,39 @@ class ToDoList extends Component {
 }
 
 function List(props){
+    let [inpValue, setInpValue] = React.useState("");
+    let [list, setList] = React.useState([]);
+
+    function addTask(inp){
+        setInpValue(inp)
+
+        list.push(
+            <li key={inp} >
+                {inp}{" "}
+                <button onClick={handleBtn(inp)}><Emoji symbol="🗑️" label="delete"/></button>
+            </li>
+        );
+
+        console.log(list);
+    }
+
+    function handleBtn(id){
+        console.log(id);
+    }
+
+    function handleInpChange(event){
+        if (event.key === 'Enter'){
+            addTask(event.target.value);
+            event.target.value = "";
+        }
+    }
 
     return (
         <div>
-            <p>Hello, World!</p>
+            <input id="inpBox" type="text" onKeyDown={handleInpChange}></input>
+            <ul>
+                {list}
+            </ul>
         </div>
     );
 }
